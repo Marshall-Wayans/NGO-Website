1dfe51129ffd3cea004f035f05a63a65b9acb4e1
Marshal Anzugila <anzugila@kali.localdomain>
2025-11-11 20:38:39 +0200

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Marshall-Wayans/NGO-Website/settings/pages"
+  base: '/NGO-Website/', // 👈 must match your repo name exactly
 })
